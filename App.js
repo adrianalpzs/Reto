c94713f1d47adfe3a1c403802f619065995ae7cc
Adriana Lpz <59716935+adrianalpzs@users.noreply.github.com>
2022-09-12 18:54:46 -0500

    pantalla home, 1 y 2

--- App.js
+++ App.js
@@ -1,10 +1,9 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 import {NavigationContainer} from "@react-navigation/native";
 import {createNativeStackNavigator} from "@react-navigation/native-stack"
 import {TailwindProvider} from "tailwindcss-react-native";
 import HomeScreen from './screens/HomeScreen';
-
+import Screen1 from "./screens/Screen1";
+import Screen2 from "./screens/Screen2";
 
 const Stack = createNativeStackNavigator();
 
@@ -13,20 +12,20 @@ export default function App() {
     <NavigationContainer>
       <TailwindProvider>
         <Stack.Navigator>
-              <Stack.Screen name='EQUIPO 5' component={HomeScreen}/>
+          <Stack.Screen name='EQUIPO 5' component={HomeScreen}/>
+          <Stack.Screen name='Screen 1' component={Screen1}
+            options={{presentation: 'modal', headerShown: false}}/>
+          <Stack.Screen name='Screen 2' component={Screen2}
+            options={{presentation: 'modal', headerShown: false}}/>
+
         </Stack.Navigator>
       </TailwindProvider>
     </NavigationContainer>
   );
 }
-
 /*
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+<Stack.Screen name='Screen 1' component={Screen1}
+options={{presentation: 'modal', headerShown: false}}/>
+<Stack.Screen name='Screen 2' component={Screen2}
+options={{presentation: 'modal', headerShown: false}}/>
 */
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,61 +1,122 @@
 import React from 'react'
 import { SafeAreaView, View, Text, TouchableOpacity, Image, TextInput} from 'react-native'
-//import { useNavigation } from '@react-navigation/native';
+import { ArrowLeftIcon, PresentationChartLineIcon } from 'react-native-heroicons/solid';
+import { useNavigation } from '@react-navigation/native';
 
 export default function HomeScreen() {
-    //const navigation = useNavigation();
+    const navigation = useNavigation();
 
 
     return (
         <SafeAreaView className="bg-blue">
             {/* Header */}
-            <View className="flex-row items-center mx-4 space-x-4 mb-4">
+            <View className="flex-column space-x-2 mx-4 mb-4 p-2">
                 
-                <Image 
-                    source={{uri: 'http://links.papareact.com/wru'}} 
-                    className="h-12 w-12 bg-gray-300 p-4 rounded-full"
-                />
-                <View className="flex-1">
-                    <Text className="font-bold text-xl">Integrantes</Text>
-                    <View className="flex-row items-center justify-between">
-                        <Text className="text-red">abc</Text>
+                <Text className="font-bold text-xl text-gray-700">Integrantes</Text>
+                <Text></Text>
+
+
+                <View className="flex-row items-center space-x-14">
+                    <View className='flex-column w-20'>
+                    {/* Yo */}
+                    
+                        <View className="flex-row items-center justify-between">
+                            <Text className="text-gray-500 font-bold w-13 text-center">Adriana Inés López Sánchez</Text>
+                        </View>
+                        <Text></Text>
+                        <Image 
+                            source={ require('../assets/adriana.jpg') }
+                            className="h-[80px] w-[80px] bg-gray-300 p-4 rounded-full"/>
                     </View>
+                    
+
+                    {/* Paola */}
+                    <View className='flex-column w-20'>
+
+                        <View className="flex-row items-center justify-between">
+                            <Text className="text-gray-500 font-bold w-13 text-center">Lidia Paola Díaz Ramírez</Text>
+                        </View>
+                        <Text></Text>
+                        <Image 
+                            source={ require('../assets/paola.jpg') }
+                            className="h-[80px] w-[80px] bg-gray-300 p-4 rounded-full"/>
+                    </View>
+
+
+                    {/* Majo */}
+                    <View className='flex-column w-20'>
+                        <View className="flex-row items-center justify-center">
+                            <Text className="text-gray-500 font-bold w-13 text-center">María José Mendoza Muñiz </Text>
+                        </View>
+                        <Text></Text>
+                        <Image 
+                            source={ require('../assets/adri.jpg') }
+                            className="h-[80px] w-[80px] bg-gray-300 p-4 rounded-full"/>
+                    </View>
+
                 </View>
-
-                <View className="flex-1">
-                    <Text className="font-bold text-xl">Descripción del Problema</Text>
-                    <View className="flex-row items-center justify-between">
-                        <Text className="text-gray-400">abc</Text>
-                    </View>
-                </View>
-
-                <View className="flex-1">
-                    <Text className="font-bold text-xl">Importancia Problema</Text>
-                    <View className="flex-row items-center justify-between">
-                        <Text className="text-gray-400">abc</Text>
-                    </View>
-                </View>
-
-
-
-                <TouchableOpacity 
-                    onPress={() => navigation.navigate('Screen1')}
-                    className="mx-1 rounded-full">
-                    <View className="p-1" >
-                        
-                    </View>
-                </TouchableOpacity>
-
             </View>
 
-             {/* EEEEE */}
+
+             {/* Descripción */}
             <View className="flex-row items-center space-x-2 mx-4 mb-4 ">
-                <View className="flex-row flex-1 space-x-2 p-2 bg-gray-100" style={{borderRadius: 20}}>
-                    <TextInput 
-                        className="ml-4 mr-4 flex-1" 
-                    />
+                <View className="flex-column flex-1 space-x-2 p-2 space-y-3 bg-gray-100" style={{borderRadius: 20}}>
+
+                    <Text className="font-bold text-xl text-gray-700">Descripción del Problema</Text>
+                    <View className="flex-row items-center justify-between">
+                        <Text className="text-gray-600">Aquí va la descripción</Text>
+                    </View>
+                  
                 </View>
             </View>
+
+            {/* Importancia */}
+             <View className="flex-row items-center space-x-2 mx-4 mb-4">
+                <View className="flex-column flex-1 space-x-2 space-y-3 p-2 bg-gray-100" style={{borderRadius: 20}}>
+
+                <Text className="font-bold text-xl text-gray-700">Importancia Problema</Text>
+                    <View className="flex-row items-center justify-between">
+                        <Text className="text-gray-600">Aquí va la importancia</Text>
+                    </View>
+                  
+                </View>
+            </View>
+
+
+            {/* Pantallas de los resultados */}
+            <Text></Text>
+            <View className="flex-row items-center justify-center space-x-4 ">
+                {/* Pantalla 1  - RESULTADOS 1ER ALGORITMO */}
+                <View className=" flex-column w-40 items-center">
+                    <View className="flex-row justify-center items-center">
+                        <Text className="text-gray-600 text-center font-bold w-21">RESULTADOS 1º ALGORITMO</Text>
+                    </View>
+                    <TouchableOpacity 
+                                onPress={() => navigation.navigate('Screen 1')}
+                                className="absolute top-5 left-8 p-10">
+                                <PresentationChartLineIcon size={20} color="gray" />
+                    </TouchableOpacity>
+                </View>
+
+
+                {/* Pantalla 2  - RESULTADOS 2DO ALGORITMO*/}
+                <View className=" flex-column w-40 items-center">
+                    <View className="flex-row justify-center items-center">
+                        <Text className="text-gray-600 text-center font-bold w-21">RESULTADOS 2º ALGORITMO</Text>
+                    </View>
+                    <TouchableOpacity 
+                                onPress={() => navigation.navigate('Screen 2')}
+                                className="absolute top-5 right-8 p-10">
+                                <PresentationChartLineIcon size={20} color="gray" />
+                    </TouchableOpacity>
+                </View>
+            
+            </View>
+
+
+            
+
+
 
         </SafeAreaView>
     )
