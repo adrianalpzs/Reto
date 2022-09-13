79df88b96ad0bcd7d4c8f2e26fb1acc8118f45cd
Adriana Lpz <59716935+adrianalpzs@users.noreply.github.com>
2022-09-13 13:01:51 -0500

    Screen 1 & 2

--- screens/Screen1.js
+++ screens/Screen1.js
@@ -1,4 +1,4 @@
-import { View, Text, ImageBackground } from 'react-native'
+import { View, Text, ImageBackground, ScrollView, SafeAreaView, Image } from 'react-native'
 import React from 'react'
 
 export default function Screen1() {
@@ -6,11 +6,110 @@ export default function Screen1() {
     <ImageBackground
             source={require('../assets/fondocc.jpg')}
             style={{width: '100%', height: '120%'}}
-            className='opacity-70'
+            className='opacity-90'
             > 
-    <View>
-      <Text className='text-white'>Screen1</Text>
-    </View>
+    <SafeAreaView>
+      <ScrollView
+        className="flex-1 px-2 py-4" 
+        contentContainerStyle={{paddingBottom: 350}}
+        >
+          <View className="flex-grow space-x-2 mx-4 mb-4">
+            <Text className='h-8'></Text>
+            <Text className="font-bold text-4xl text-white">Algoritmo 1</Text>
+            <Text className="h-2"></Text>
+            <Text className="text-xl font-light text-gray-300">Desition Tree</Text>
+            <Text className='h-5'></Text>
+            
+            {/* Justificación */}
+            <View className="flex-row items-center space-x-2 mx-4 mb-4">
+                    <View className="flex-column flex-1 space-x-2 p-2 space-y-3 bg-gray-100 opacity-60" style={{borderRadius: 20}}>
+
+                        <Text className="font-bold text-xl text-gray-900">Justificación</Text>
+                        <View className="flex-row items-center justify-between">
+                            <Text className="text-gray-900 text-justify right-1">Aquí va la justificación
+                            Aquí va la justificación
+                            Aquí va la justificación
+                              Aquí va la justificación
+                              Aquí va la justificación
+                              Aquí va la justificación
+                              Aquí va la justificación
+                              Aquí va la justificación
+                              Aquí va la justificación
+                              Aquí va la justificación
+                            </Text>
+                        </View>
+                    </View>
+              </View>
+
+              {/* Resultados */}
+              <View>
+                <Text className='h-8'></Text>
+                <Text className="font-bold text-3xl text-white">Resultados</Text>
+                <Text className="h-4"></Text>
+
+                {/* Presición Obtenida */}
+                <View>
+                  <Text className="text-xl font-semibold text-gray-300">Presición Obtenida</Text>
+                  <Text className='h-3'></Text>
+                  <Image 
+                  source={ require('../assets/adriana.jpg') }
+                  className="h-[300px] w-[350px]"
+                  />
+                </View>
+                {/* Matriz de Confusión */}
+                <View>
+                  <Text className='h-10'></Text>
+                  <Text className="text-xl font-semibold text-gray-300">Matriz de Confusión</Text>
+                  <Text className='h-3'></Text>
+                  <Image 
+                  source={ require('../assets/adriana.jpg') }
+                  className="h-[300px] w-[350px]"
+                  />
+                </View>
+
+                {/* ROC */}
+                <View>
+                  <Text className='h-10'></Text>
+                  <Text className="text-xl font-semibold text-gray-300">ROC</Text>
+                  <Text className='h-3'></Text>
+                  <Image 
+                  source={ require('../assets/adriana.jpg') }
+                  className="h-[300px] w-[350px]"
+                  />
+                </View>
+
+                {/* AUC */}
+                <View>
+                  <Text className='h-10'></Text>
+                  <Text className="text-xl font-semibold text-gray-300">AUC</Text>
+                  <Text className='h-3'></Text>
+                  <Image 
+                  source={ require('../assets/adriana.jpg') }
+                  className="h-[300px] w-[350px]"
+                  />
+                </View>
+                {/* Kaggle */}
+                <View>
+                  <Text className='h-10'></Text>
+                  <Text className="text-xl font-semibold text-gray-300">Puntaje Obtenido en Kaggle</Text>
+                  <Text className='h-3'></Text>
+                  <Image 
+                  source={ require('../assets/adriana.jpg') }
+                  className="h-[300px] w-[350px]"
+                  />
+                </View>
+
+              </View>
+
+
+
+
+          </View>
+
+      </ScrollView>
+
+    </SafeAreaView>
+    
     </ImageBackground>
   )
 }
